335fcf0819f0903b9b889f7b01912465507b31b0
Jamie Davenport <jamiedav97@gmail.com>
2019-10-19 14:38:38 +0100

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    title: `Jamie Davenport`,
+    author: `Jamie Davenport`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://jamiedavenport.dev/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `jdavenport97`,
     },
   },
   plugins: [
