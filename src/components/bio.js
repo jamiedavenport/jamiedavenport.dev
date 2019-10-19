78c12f3c8311b62e95a18dfd7dd6e5e733004147
Jamie Davenport <jamiedav97@gmail.com>
2019-10-19 16:36:19 +0100

    Add key details

--- src/components/bio.js
+++ src/components/bio.js
@@ -11,7 +11,6 @@ import React from "react"
 import styled from "styled-components"
 import { rhythm } from "../utils/typography"
 
-
 function Bio() {
   return (
     <StaticQuery
@@ -24,22 +23,22 @@ function Bio() {
               fixed={data.avatar.childImageSharp.fixed}
               alt={author}
               style={{
-                marginRight: rhythm(1 / 2),
+                marginRight: rhythm(1),
                 marginBottom: 0,
-                minWidth: 50,
+                minWidth: 100,
                 borderRadius: `100%`,
               }}
               imgStyle={{
                 borderRadius: `50%`,
               }}
             />
-            <p>
-              Written by <strong>{author}</strong> who lives and works in Manchester, UK building useful things.
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
-            </p>
+            <div>
+              <div>Software Engineer</div>
+              <div>Manchester, UK</div>
+              <div>
+                <a href={`https://twitter.com/${social.twitter}`}>Twitter</a>
+              </div>
+            </div>
           </Container>
         )
       }}
@@ -51,7 +50,7 @@ const bioQuery = graphql`
   query BioQuery {
     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
       childImageSharp {
-        fixed(width: 50, height: 50) {
+        fixed(width: 100, height: 100) {
           ...GatsbyImageSharpFixed
         }
       }
