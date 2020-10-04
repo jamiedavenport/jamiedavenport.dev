b646d4e562afbb0fc73e080e74d9b6313fca38f6
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-10-04 15:19:02 +0100

    Init NextJS app

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      flex: {
+        2: "2 2 0%",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
