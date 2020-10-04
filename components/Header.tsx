a84f81eaa04e85732b5d1dc5ef0e7abc7c06b5cc
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-10-04 18:05:39 +0100

    Add MobileNavButton

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DesktopNav from "./DesktopNav";
 import MobileNav from "./MobileNav";
+import MobileNavButton from "./MobileNavButton";
 
 const Header: React.FC = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -13,22 +14,10 @@ const Header: React.FC = () => {
           <DesktopNav />
         </div>
         <div className="md:hidden">
-          <button onClick={() => setIsNavOpen(!isNavOpen)}>
-            <svg
-              className="block h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
+          <MobileNavButton
+            isOpen={isNavOpen}
+            onClick={() => setIsNavOpen(!isNavOpen)}
+          />
         </div>
       </div>
       <div className="md:hidden">
---
+++ components/MobileNavButton.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+type Props = {
+  isOpen: boolean;
+  onClick: () => void;
+};
+
+const MobileNavButton: React.FC<Props> = ({ isOpen, onClick }) => {
+  let icon = (
+    <svg
+      className="block h-6 w-6"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  );
+  if (isOpen) {
+    icon = (
+      <svg
+        className="h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    );
+  }
+
+  return <button onClick={onClick}>{icon}</button>;
+};
+
+export default MobileNavButton;
