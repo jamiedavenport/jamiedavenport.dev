3d88b6febd653545899445d64dd1cbe8d03b5b02
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-10-04 18:57:36 +0100

    Add Intro component

---
+++ components/Intro.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Intro: React.FC = () => {
+  return (
+    <div className="text-2xl">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur harum
+      laborum distinctio nulla molestiae, non eaque est autem eius cupiditate
+      facere dolor molestias accusamus nostrum esse quis dolore, ullam qui.
+    </div>
+  );
+};
+
+export default Intro;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
 import Footer from "components/Footer";
 import Header from "components/Header";
+import Intro from "components/Intro";
 import React from "react";
 
 const Home: React.FC = () => {
   return (
     <div className="container mx-auto space-y-10">
       <Header />
-      <div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
-        pariatur quos vel! Aut, ratione ipsum enim accusamus nulla nisi
-        aspernatur impedit provident. Eligendi perferendis sed commodi placeat
-        tempora hic nemo?
-      </div>
+      <Intro />
       <Footer />
     </div>
   );
