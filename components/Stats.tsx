37028bfb2318b8f994c9aa0ecae7e72e8cd15a17
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-10-05 19:38:53 +0100

    Add a Stats component

---
+++ components/Stats.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Divider from "./Divider";
+
+type StatProps = {
+  title: string;
+  value: string;
+};
+
+const Stat: React.FC<StatProps> = ({ title, value }) => {
+  return (
+    <div className="flex flex-col flex-1 items-center font-mono space-y-2">
+      <div className="text-5xl font-black leading-none">{value}</div>
+      <div className="text-gray-600 leading-none">{title}</div>
+    </div>
+  );
+};
+
+const Stats: React.FC = () => {
+  return (
+    <div className="flex flex-col space-y-20 md:space-y-0 md:flex-row bg-gray-300 rounded px-5 py-16 md:py-10">
+      <Stat title="Repositories" value="21" />
+      <Stat title="Contributed To" value="3" />
+      <Stat title="Total PRs" value="9" />
+    </div>
+  );
+};
+
+export default Stats;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Intro from "components/Intro";
 import Head from "next/head";
 import WIP from "components/WIP";
 import Section from "components/Section";
+import Stats from "components/Stats";
 
 const Home: React.FC = () => {
   return (
@@ -12,18 +13,23 @@ const Home: React.FC = () => {
       <Head>
         <title>Jamie Davenport</title>
       </Head>
-      <div className="container mx-auto space-y-20 p-4">
-        <Header />
+      <div className="min-h-screen bg-gray-100">
+        <div className="container mx-auto space-y-20 p-4">
+          <Header />
 
-        <Section title="Welcome">
-          <Intro />
-        </Section>
+          <Section title="Welcome">
+            <div className="space-y-10">
+              <Intro />
+              <Stats />
+            </div>
+          </Section>
 
-        <Section title="Blog">
-          <WIP />
-        </Section>
+          <Section title="Blog">
+            <WIP />
+          </Section>
 
-        <Footer />
+          <Footer />
+        </div>
       </div>
     </>
   );
