4fe6b22d222c1ee06a854eaa329d44a76f507d4e
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-10-08 14:43:37 +0100

    Add Fathom tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from "next/app";
+import * as Fathom from "fathom-client";
 
 import "../styles/tailwind.css";
+import { useRouter } from "next/dist/client/router";
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("EAZZKNYE", {
+      includedDomains: ["jamiedavenport.dev"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
