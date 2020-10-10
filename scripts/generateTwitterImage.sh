bf877477cf5cf5ba6ab652ee3c99c99b9fd81ee8
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-10-10 12:01:47 +0100

    Update test-image

--- scripts/generateTwitterImage.sh
+++ scripts/generateTwitterImage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 title=$(sed 's/ /%20/g' <<< $1)
-"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --headless --screenshot --window-size=560,300 https://jamiedavenport.dev/util/twitter?title=$title
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --headless --screenshot --window-size=280,150 https://jamiedavenport.dev/util/twitter?title=$title
