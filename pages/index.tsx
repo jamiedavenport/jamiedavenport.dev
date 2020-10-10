675f348df295571c44bd88cda52c95b52a8bd48d
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-10-10 12:24:46 +0100

    Add Twitter and OG meta tags to the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Intro from "components/Intro";
 import Section from "components/Section";
-import Stats from "components/Stats";
 import Layout from "components/Layout";
 import PostList from "components/PostList";
 import { GetStaticProps } from "next";
 import { getPosts, PostMeta } from "lib/posts";
 import WIP from "components/WIP";
+import Head from "next/head";
 
 type Props = {
   posts: PostMeta[];
@@ -23,8 +23,24 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 };
 
 const Home: React.FC<Props> = ({ posts }) => {
+  const description = `Software Engineer based in the UK`;
+  const imageUrl = "https://jamiedavenport.dev/favicon.png";
+
   return (
     <Layout title="Jamie Davenport">
+      <Head>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@jdavenport97" />
+        <meta name="twitter:creator" content="@jdavenport97" />
+        <meta name="twitter:title" content="Jamie Davenport" />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={imageUrl} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Jamie Davenport" />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={imageUrl} />
+        <meta name="description" content={description} />
+      </Head>
       <div className="space-y-20">
         <Section title="Welcome">
           <div className="space-y-16">
