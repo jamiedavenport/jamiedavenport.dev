1922636df4b88a74102015eca4063ee142160b94
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-09 22:05:43 +0000

    Simplify tag colours

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,4 +1,3 @@
-import { stringToTailwindColour } from 'lib/colour'
 import React from 'react'
 
 interface Props {
@@ -6,12 +5,8 @@ interface Props {
 }
 
 const Tag: React.FC<Props> = ({ tag }) => {
-  const colour = stringToTailwindColour(tag)
-
   return (
-    <span
-      className={`bg-${colour}-500 border border-${colour}-600 text-${colour}-200 text-xs py-1 px-2 rounded-xl font-bold`}
-    >
+    <span className="bg-gray-800 text-white text-xs py-1 px-2 rounded-xl font-bold">
       {tag}
     </span>
   )
