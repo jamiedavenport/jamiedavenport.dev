dc29bd518d11661b1463ca1bc0436c5cd5af305d
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-09 22:00:13 +0000

    Fix hashCode

--- lib/hashCode.ts
+++ lib/hashCode.ts
@@ -1,7 +1,8 @@
 const hashCode = (s: string): number => {
-  let h = 0
-  const l = s.length,
+  let h = 0,
     i = 0
+  const l = s.length
+
   if (l > 0) while (i < l) h = ((h << 5) - h + s.charCodeAt(i++)) | 0
   return h
 }
