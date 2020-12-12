999d8fb1a9c771936fd8e0a0b72e1104fe4b3449
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-12 14:28:59 +0000

    Add dark mode toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colours = require('tailwindcss/colors')
 
 module.exports = {
-  darkMode: 'media',
+  darkMode: 'class',
   important: true,
   purge: {
     content: [
