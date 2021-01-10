7a322a38c5ed87b9ed9da1705d4ba7a1fbadd2b8
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2021-01-10 07:59:04 +0000

    HYHO Volta? (#13)

--- components/blog/components.ts
+++ components/blog/components.ts
@@ -1,5 +1,12 @@
 import Achievement from './Achievement'
+import dynamic from 'next/dynamic'
+import Link from './Link'
+
+// TODO: Is there a better way around this?
+const RepoCard = dynamic(() => import('./RepoCard'), { ssr: false })
 
 export default {
   Achievement: Achievement,
+  RepoCard: RepoCard,
+  a: Link,
 }
