5ac30914bfce652ed17ee9370cc8c3ae11f5c62b
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2021-01-31 12:46:47 +0000

    Unshuffle stack

--- pages/stack.tsx
+++ pages/stack.tsx
@@ -3,13 +3,11 @@ import StackItem from 'components/StackItem'
 import { stack } from 'lib/stack'
 import React from 'react'
 
-const shuffledStack = stack.sort(() => 0.5 - Math.random())
-
 const StackPage: React.FC = () => {
   return (
     <Layout title="Stack">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-max gap-5 md:px-5">
-        {shuffledStack.map((tool) => (
+        {stack.map((tool) => (
           <StackItem key={tool.name} tool={tool} />
         ))}
       </div>
