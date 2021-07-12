485bcbda76c0e722d34f5b9690cb31754a05a864
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2021-07-12 21:46:35 +0100

    Add header, footer and logo to the basic layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,22 @@
+import React from "react";
+import Logo from "./Logo";
+
 type Props = {
-  children?: React.ReactElement;
+  children?: React.ReactNode;
 };
 
 export default function Layout({ children }: Props) {
   return (
-    <>
-      <header>Header</header>
+    <div className="p-4 max-w-4xl mx-auto space-y-10">
+      <header className="flex flex-row justify-between items-center">
+        <Logo />
+        <nav>Navigation</nav>
+      </header>
       <main>{children}</main>
-      <footer>Footer</footer>
-    </>
+      <footer className="flex flex-row justify-between items-center">
+        <p>Made with ❤️ in London 🇬🇧</p>
+        <div>Social Icons</div>
+      </footer>
+    </div>
   );
 }
