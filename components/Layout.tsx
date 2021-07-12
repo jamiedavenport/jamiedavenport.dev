70ebbb9d370e7d0a4058f9ef3aba750b4e66afb8
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2021-07-12 22:08:03 +0100

    Add blog and project pages and navigation links

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "./Logo";
+import Link from "next/link";
 
 type Props = {
   children?: React.ReactNode;
@@ -9,8 +10,30 @@ export default function Layout({ children }: Props) {
   return (
     <div className="p-4 max-w-4xl mx-auto space-y-10">
       <header className="flex flex-row justify-between items-center">
-        <Logo />
-        <nav>Navigation</nav>
+        <Link href="/">
+          <a>
+            <Logo />
+          </a>
+        </Link>
+        <nav>
+          <ul className="flex flex-row space-x-4">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog">
+                <a>Blog</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/projects">
+                <a>Projects</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
       <main>{children}</main>
       <footer className="flex flex-row justify-between items-center">
